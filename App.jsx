ce164f71049d39b8e870f0c2ec3202bbe5f48f30
hamzafarhani <farhani.hamza@espri.tn>
2024-10-22 12:19:58 +0200

    SplashScreen

--- App.jsx
+++ App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './src/screen/WelcomeScreen';
 import SignupScreen from './src/screen/SignupScreen';
 import LoginScreen from './src/screen/LoginScreen';
+import { Platform } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    if (Platform.OS === 'android' && SplashScreen) {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 4000); 
+    }
+  }, []);
+  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
